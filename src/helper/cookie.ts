69f8c2beb11400551c73a96bd9cbdf3dea048286
truongnx081294 <truongnx081294@gmail.com>
2025-04-22 22:42:46 +0700

    ádasdasdasd

--- src/helper/cookie.ts
+++ src/helper/cookie.ts
@@ -1,39 +1,40 @@
-import { useCookies } from "vue3-cookies";
+// import { useCookies } from "vue3-cookies";
 
-const { cookies } = useCookies();
-export const setCookie = (
-  name: string,
-  value: string,
-  days?: number | Date
-) => {
-  let expires = "";
-  if (days) {
-    if (days instanceof Date) {
-      expires = "; expires=" + days.toUTCString();
-    } else {
-      const date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-  }
+// const { cookies } = useCookies();
 
-  console.log(expires);
+// export const setCookie = (
+//   name: string,
+//   value: string,
+//   days?: number | Date
+// ) => {
+//   let expires = "";
+//   if (days) {
+//     if (days instanceof Date) {
+//       expires = "; expires=" + days.toUTCString();
+//     } else {
+//       const date = new Date();
+//       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+//       expires = "; expires=" + date.toUTCString();
+//     }
+//   }
 
-  return { cookies };
-  // document.cookie = name + '=' + (value || '') + expires + '; path=/';
-  cookies.set(name, value);
-};
+//   console.log(expires);
 
-export const getCookie = (name: string) => {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-};
-export const eraseCookie = (name: string) => {
-  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-};
+//   return { cookies };
+//   // document.cookie = name + '=' + (value || '') + expires + '; path=/';
+//   cookies.set(name, value);
+// };
+
+// export const getCookie = (name: string) => {
+//   const nameEQ = name + "=";
+//   const ca = document.cookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) == " ") c = c.substring(1, c.length);
+//     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+//   }
+//   return null;
+// };
+// export const eraseCookie = (name: string) => {
+//   document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+// };

e588840dc3627f27c32978ea3958dbd94cd0a7c6
truongnx081294 <truongnx081294@gmail.com>
2025-04-22 15:42:18 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,14 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vite.dev/config/
 export default defineConfig({
+  
   build: {
     target: 'esnext', // hoặc 'esnext', 'modules', v.v.
+  },
+  server: {
+    preview: {
+      allowedHosts: ['tonytruong.id.vn', 'localhost', '127.0.0.1'],
+    },
   },
   plugins: [vue(),
     Components({

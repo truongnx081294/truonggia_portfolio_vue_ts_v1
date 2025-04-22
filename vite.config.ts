7295eaa533176257a6e76629dc96f9d035793ed4
truongnx081294 <truongnx081294@gmail.com>
2025-04-22 15:43:46 +0700

    update allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
-import Components from 'unplugin-vue-components/vite'
+import Components from "unplugin-vue-components/vite";
 // import MotionResolver from 'motion-v/resolver'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  
   build: {
-    target: 'esnext', // hoặc 'esnext', 'modules', v.v.
+    target: "esnext", // hoặc 'esnext', 'modules', v.v.
   },
   server: {
-    preview: {
-      allowedHosts: ['tonytruong.id.vn', 'localhost', '127.0.0.1'],
-    },
+    allowedHosts: ["tonytruong.id.vn", "localhost", "127.0.0.1"],
   },
-  plugins: [vue(),
+  plugins: [
+    vue(),
     Components({
       dts: true,
       resolvers: [
